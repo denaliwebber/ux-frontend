9c4a3140284b137a9297d7bdeab0f0c362258fa4
denali.webber <denali.webber@gmail.com>
2020-11-11 07:59:13 -0800

    Added fixed hovering functions for navigation pane.

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-var books = document.querySelector('#book-container')
+const books = document.querySelector('#book-container')
+const dashboard = document.querySelector('#dashboard-container')
+const calendar = document.querySelector('#calendar-container')
 
 books.addEventListener('mouseover', function (event) {
-  event.target.style.backgroundColor = '#004718'
+  books.style.backgroundColor = '#004718'
 })
 
 books.addEventListener('mouseout', function (event) {
-  event.target.style.backgroundColor = '#005c02'
+  books.style.backgroundColor = '#005c02'
+})
+
+dashboard.addEventListener('mouseover', function (event) {
+  dashboard.style.backgroundColor = '#004718'
+})
+
+dashboard.addEventListener('mouseout', function (event) {
+  dashboard.style.backgroundColor = '#005c02'
+})
+
+calendar.addEventListener('mouseover', function (event) {
+  calendar.style.backgroundColor = '#004718'
+})
+
+calendar.addEventListener('mouseout', function (event) {
+  calendar.style.backgroundColor = '#005c02'
 })
